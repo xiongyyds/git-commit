0c48e234efed72d1d59c1de2fb974e012148fa74
dafage <dafage@.com>
2022-02-15 14:05:03 +0800

    2

--- src/com/hebau/GIT.java
+++ src/com/hebau/GIT.java
@@ -3,5 +3,6 @@ package com.hebau;
 public class GIT {
     public static void main(String[] args) {
         System.out.println("git--000000000");
+        System.out.println("111111111111111");
     }
 }
