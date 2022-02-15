f7a443f036c755df2775a7e747067649ded79d31
xiongyyds <88039825+xiongyyds@users.noreply.github.com>
2022-02-15 14:06:33 +0800

    Update GIT.java

--- src/com/hebau/GIT.java
+++ src/com/hebau/GIT.java
@@ -4,5 +4,6 @@ public class GIT {
     public static void main(String[] args) {
         System.out.println("git--000000000");
         System.out.println("111111111111111");
+        System.out.println("2222222222222222");
     }
 }
